4acd3fab849b84214d7fc965060ea0b3cc6c8d4f
Xinzhu-Fang <xf15@hampshire.edu>
2019-10-24 15:45:23 -0400

    org

--- create_exp.py
+++ create_exp.py
@@ -1,5 +1,5 @@
 # dput(sort(unique()))
-# Three navtive speaekers were asked to classifiy sex for these namees into Female, Male, and Ambiguous. For each name, if at least two people both chose F of M and the third one chose A, the name is put in the majority sex category, otherwise the name is discarded. 
+# Three navtive speaekers were asked to classifiy sex for these namees into Female (F), Male (M), and Ambiguous (A). For each name, if at least two people both chose F of M and the third one chose A, the name is put in the majority sex category, otherwise the name is discarded. 
 
 # In order to have the filler questions being the same indepdent of the correction condition, the "wrong" list which is the question list is the same acroos exps' on control trials, the correct version is the same as the "wrong" version -- the "wrong" is actaully correct.
 

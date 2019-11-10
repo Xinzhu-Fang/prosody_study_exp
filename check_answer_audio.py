2bbeed6d4c9172afb02310db40968aefe46c6b09
Xinzhu-Fang <xf15@hampshire.edu>
2019-11-10 15:34:01 -0500

    check answer working

--- check_answer_audio.py
+++ check_answer_audio.py
@@ -1,57 +1,68 @@
-run this file at two stages:
-    1: get the auto fail
+#run this file at two stages:
+#    1: get the auto fail. Then enter the trial num to reject
+#    2: remove the rejected trial to output the final tTranscript
 
+bStage = 1
+bStage = 2 #manual
+reject_row_index = [3,4] #manual
 import pandas as pd
 import os
 import speech_recognition as sr
 
-cur_exp = 'exp1'
-tAll_trials = pd.read_csv(os.path.join(cur_exp, 'tAll_trials.csv'))
+if bStage == 1:
+    cur_exp = 'exp1'
+    tAll_trials = pd.read_csv(os.path.join(cur_exp, 'tAll_trials.csv'))
 
-supposed_answer_transcript = []
-auto_transcript = []
-status = []
-trial_id = []
-answer_files = []
-for iFile in os.listdir(os.path.join('/Users/xzfang/Desktop/prosody_study_data', cur_exp, 'responses_to_analyze')):
-    #    print(cur_file)
-    cur_file = os.path.join('/Users/xzfang/Desktop/prosody_study_data', cur_exp, 'responses_to_analyze', iFile)
-    cur_trial = int(cur_file[len(cur_file) - 6: len(cur_file) - 4])
-    print(cur_trial)
-    trial_id = trial_id + [cur_trial]
-    supposed_answer_transcript = supposed_answer_transcript + [str(x) for x in tAll_trials.loc[tAll_trials.trial_id == cur_trial, 'answer_script']]
-    answer_files = answer_files + [cur_file]
-    AUDIO_FILE = cur_file
-    # use the audio file as the audio source
-    r = sr.Recognizer()
-    with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file
-        # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        script = r.recognize_google(audio)
-        auto_transcript = auto_transcript + [script]
+    supposed_answer_transcript = []
+    auto_transcript = []
+    status = []
+    trial_id = []
+    answer_files = []
+    for iFile in os.listdir(os.path.join('/Users/xzfang/Desktop/prosody_study_data', cur_exp, 'responses_to_analyze')):
+        #    print(cur_file)
+        cur_file = os.path.join('/Users/xzfang/Desktop/prosody_study_data', cur_exp, 'responses_to_analyze', iFile)
+        cur_trial = int(cur_file[len(cur_file) - 6: len(cur_file) - 4])
+        print(cur_trial)
+        trial_id = trial_id + [cur_trial]
+        supposed_answer_transcript = supposed_answer_transcript + [str(x) for x in tAll_trials.loc[tAll_trials.trial_id == cur_trial, 'answer_script']]
+        answer_files = answer_files + [cur_file]
+        AUDIO_FILE = cur_file
+        # use the audio file as the audio source
+        r = sr.Recognizer()
+        with sr.AudioFile(AUDIO_FILE) as source:
+            audio = r.record(source)  # read the entire audio file
+            # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            script = r.recognize_google(audio)
+            auto_transcript = auto_transcript + [script]
 
-        if not script == tAll_trials.loc[tAll_trials.trial_id == cur_trial, 'answer_script'].any():
-    #                print(iRow.question_file + ' is fine')
-    #            else:
-    #                print('check file ' + )
-            status = status + ['auto_fail']
-        else:
-            status = status + ['auto_pass']
+            if not script == tAll_trials.loc[tAll_trials.trial_id == cur_trial, 'answer_script'].any():
+        #                print(iRow.question_file + ' is fine')
+        #            else:
+        #                print('check file ' + )
+                status = status + ['auto_fail']
+            else:
+                status = status + ['auto_pass']
 
-# question_files = question_files + [  dquestion_file]
-#                print(iRow.question_file + 'recognized as' + script)
-# print("Google Speech Recognition thinks you said " + )
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request result from Google Speech Recognition service; {0}".format(e))
+    # question_files = question_files + [  dquestion_file]
+    #                print(iRow.question_file + 'recognized as' + script)
+    # print("Google Speech Recognition thinks you said " + )
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request result from Google Speech Recognition service; {0}".format(e))
 
-tTranscript = pd.DataFrame(zip(trial_id, supposed_answer_transcript, auto_transcript, status, answer_files), columns=['trial_id', 'supposed_answer_transcript', 'auto_transcript', 'status', 'answer_files'])
+    tTranscript = pd.DataFrame(zip(trial_id, supposed_answer_transcript, auto_transcript, status, answer_files), columns=['trial_id', 'supposed_answer_transcript', 'auto_transcript', 'status', 'answer_files'])
 
-tTranscript_auto_fail = tTranscript.loc[tTranscript.status == 'auto_fail']
+    tTranscript_auto_fail = tTranscript.loc[tTranscript.status == 'auto_fail']
 
-tTranscript.to_csv('tTranscript_' + cur_exp + '.csv',  encoding='utf-8', index=False)
+    tTranscript_auto_fail.to_csv('tTranscript_auto_fail_' + cur_exp + '.csv',  encoding='utf-8')
+
+else:
+    
+    tTranscript = tTranscript.drop(reject_row_index)
+
+    tTranscript.to_csv('tTranscript_' + cur_exp + '.csv',  encoding='utf-8')

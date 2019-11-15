b67e8627f0f9fe3c5855eac997e3a0b82b01de21
Xinzhu-Fang <xf15@hampshire.edu>
2019-11-15 16:45:40 -0500

    check names fixed, looked at wrong dict

--- check_words_in_model_dict.py
+++ check_words_in_model_dict.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import re
 names = ["Nancy", "Vicky", "Tori", "Penny", "Jilly", "Cindy", "Laurie",
 "Franny", "Brandy", "Betty", "Kylie", "Carrie", "Tracy", "Chloe",
 "Annie", "Mindy", "Molly", "Sally", "Lacey", "Carlie", "Mandy",
@@ -13,14 +14,28 @@ verbs = ["Kissing", "Kicking", "Poking", "Lifting", "Pushing", "Pulling"]
 status = []
 words = names + verbs
 WORDS = [x.upper() for x in words]
-with open(os.path.join('analysis', 'model', 'dict')) as f:
-    a = f.read()
+with open(os.path.join('analysis', 'tmp', 'dict')) as f:
+    a = f.readlines()
     for iWord in WORDS:
-        if not iWord in a:
-            # print(iWord + ' is not in the dict')
+        bFound = 0
+        for iLine in a:
+            b = re.search('^' + iWord + ' ', iLine)
+#            print(b)
+#            print(iLine)
+#            if type(b.group(0)) is str:
+            if not b is None:
+                status = status + ["exist"]
+                bFound = 1
+                break
+        if not bFound:
+            
+                
+    
+#            if not ' ' + iWord + ' ' in a:
+                # print(iWord + ' is not in the dict')
             status = status + ["do not exist"]
-        else:
-            status = status + ["exist"]
+#            else:
+                
 
 tWords_in_dict = pd.DataFrame(zip(WORDS, status), columns=['WORDS', 'status'])
 tWords_in_dict.to_csv('tWords_in_dict.csv',  encoding='utf-8')
